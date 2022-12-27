fb38fd77d333e48fa6c12b9a119f911e1ea311f7
Tshirelletso <trmoliki@outlook.com>
2022-12-28 00:06:50 +0200

    shutil move instead of replace

--- renaming movie files.py
+++ renaming movie files.py
@@ -1,11 +1,12 @@
-import os, re, shutil
-from pathlib import Path
+import os, re, shutil, subprocess
+from pathlib import Path, WindowsPath
 
 moviesPath = Path.home()/"Videos"/"Movies"
 
-for item in os.listdir(moviesPath): # In movies folder
+for item in os.listdir(moviesPath): # Items in movies folder
     if os.path.isdir(moviesPath/item) and item != "Subs":
-        for subItem in os.listdir(moviesPath/item): # sub folder (e.g. Fall 2022 folder)
+
+        for subItem in os.listdir(moviesPath/item): # sub folder (e.g. inside Fall 2022 folder)
             if subItem.endswith("srt"):
                 os.replace(moviesPath/item/subItem, moviesPath/"Subs"/subItem)
             elif subItem.endswith("mp4"):
@@ -14,6 +15,8 @@ for item in os.listdir(moviesPath): # In movies folder
                 movieTitle = f"{movieName.replace('.', ' ')} ({movieYear}).mp4"
                 os.replace(moviesPath/item/subItem, moviesPath/movieTitle)
             elif os.path.isdir(moviesPath/item/subItem) and subItem == "Subs":
-                os.replace(moviesPath/item/subItem, moviesPath/"Subs"/f'{item.split(")")[0]})')
+                shutil.move(moviesPath/item/subItem, moviesPath/"Subs"/f'{item.split(")")[0]})')
         # Delete the folder after moving out it's content
-        shutil.rmtree(moviesPath/item)
+        # print(f"rmdir /s/q '{WindowsPath(moviesPath/item)}\\'")
+        # subprocess.run(f"rmdir /s/q '{moviesPath/item}\\'", shell=True)
+        # shutil.rmtree(moviesPath/item)
