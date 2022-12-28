a6a21a4c9e7a48ea0219c5ce72e810c9ee44bf22
Tshirelletso <trmoliki@outlook.com>
2022-12-28 09:22:07 +0200

    Deletes folders after moving content

--- renaming movie files.py
+++ renaming movie files.py
@@ -1,5 +1,5 @@
 import os, re, shutil, subprocess
-from pathlib import Path, WindowsPath
+from pathlib import Path
 
 moviesPath = Path.home()/"Videos"/"Movies"
 
@@ -8,15 +8,17 @@ for item in os.listdir(moviesPath): # Items in movies folder
 
         for subItem in os.listdir(moviesPath/item): # sub folder (e.g. inside Fall 2022 folder)
             if subItem.endswith("srt"):
-                os.replace(moviesPath/item/subItem, moviesPath/"Subs"/subItem)
+                # Move subs into the main Subs folder
+                shutil.move(moviesPath/item/subItem, moviesPath/"Subs"/subItem)
             elif subItem.endswith("mp4"):
+                # Move movies out to main Movies directory
                 movieYear = re.findall("\d{4}", subItem)[0]
                 movieName = re.split("\.\d{4}", subItem, 1)[0]
                 movieTitle = f"{movieName.replace('.', ' ')} ({movieYear}).mp4"
-                os.replace(moviesPath/item/subItem, moviesPath/movieTitle)
+                shutil.move(moviesPath/item/subItem, moviesPath/movieTitle)
             elif os.path.isdir(moviesPath/item/subItem) and subItem == "Subs":
+                # Move the movies Subs into the main Subs folder 
                 shutil.move(moviesPath/item/subItem, moviesPath/"Subs"/f'{item.split(")")[0]})')
-        # Delete the folder after moving out it's content
-        # print(f"rmdir /s/q '{WindowsPath(moviesPath/item)}\\'")
-        # subprocess.run(f"rmdir /s/q '{moviesPath/item}\\'", shell=True)
-        # shutil.rmtree(moviesPath/item)
+
+        # Delete movie folder after moving out it's contents
+        subprocess.run(f'rmdir /s/q "{moviesPath/item}/"', shell=True)
